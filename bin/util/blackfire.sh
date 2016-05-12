b20697d1d6fae27732ca801337e668c65904daab
davoge <davoge@users.noreply.github.com>
2016-05-11 22:13:55 -0700

    Update blackfire.sh

--- bin/util/blackfire.sh
+++ bin/util/blackfire.sh
@@ -3,13 +3,19 @@
 install_blackfire_ext() {
     # special treatment for Blackfire; we enable it if we detect a server id and a server token for it
     # otherwise users would have to have it in their require section, which is annoying in development environments
-    BLACKFIRE_SERVER_ID=${BLACKFIRE_SERVER_ID:-}
-    BLACKFIRE_SERVER_TOKEN=${BLACKFIRE_SERVER_TOKEN:-}
-    if [[ "$engine" == "php" && -n "$BLACKFIRE_SERVER_TOKEN" && -n "$BLACKFIRE_SERVER_ID" ]] && ! $engine -n $(which composer) show -d "$build_dir/.heroku/php" --installed --quiet heroku-sys/ext-blackfire 2>/dev/null; then
-        if $engine -n $(which composer) require --update-no-dev -d "$build_dir/.heroku/php" -- "heroku-sys/ext-blackfire:*" >> $build_dir/.heroku/php/install.log 2>&1; then
-            echo "- Blackfire detected, installed ext-blackfire" | indent
-        else
-            warning_inline "Blackfire detected, but no suitable extension available"
-        fi
-    fi
+yaf_version=2.3.5
+	
+
+	curl -L "http://pecl.php.net/get/yaf-${yaf_version}.tgz" \
+	    | tar xzv
+	
+
+	cd yaf-${yaf_version}
+	/app/vendor/php/bin/phpize
+	./configure --with-php-config=/app/vendor/php/bin/php-config
+	
+
+	make
+	cp modules/yaf.so "$EXT_DIR/yaf.so"
+	echo "extension=yaf.so" > "$PREFIX/etc/conf.d/yaf.ini"
 }
